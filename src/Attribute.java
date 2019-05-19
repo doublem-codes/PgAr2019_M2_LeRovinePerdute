5f2dfe67bd6c1994c90807b6da8e08e569521a16
doublem-codes <m.milanesi004@studenti.unibs.it>
2019-05-19 15:28:40 +0200

    v1.0

---
+++ src/Attribute.java
@@ -0,0 +1,29 @@
+
+public class Attribute {
+
+	private String name;
+	private String value;
+	
+	public Attribute(String name) {
+		this.setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+	
+}
